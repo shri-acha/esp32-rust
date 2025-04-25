3c6252332aec5f223abde6132244b0940a13fb97
shri-acha <happy.irhs@gmail.com>
2025-04-25 08:12:52 +0545

    Wifi(Station Mode)

--- esp32/src/main.rs
+++ esp32/src/main.rs
@@ -4,7 +4,7 @@ use esp_idf_svc::hal::{delay::Delay, gpio::PinDriver, prelude::Peripherals};
 use esp_idf_svc::nvs::EspDefaultNvsPartition;
 use heapless::String;
 
-use esp_idf_svc::wifi::{AccessPointConfiguration, AuthMethod, ClientConfiguration, Configuration, EspWifi};
+use esp_idf_svc::wifi::{AccessPointConfiguration, AuthMethod, ClientConfiguration, Configuration, EspWifi, ScanMethod};
 use esp_idf_svc::eventloop::EspSystemEventLoop;
 
 
@@ -22,18 +22,40 @@ fn main() {
 
     let mut w_driver = EspWifi::new(w_modem, w_sysloop, Some(w_nvs)).unwrap();    
     
-    let ssid = heapless::String::from_str("my_esp").expect("[ERROR - ssid]");
-    let password = heapless::String::from_str("password").expect("[ERROR - password]");
+    // let ssid = heapless::String::from_str("my_esp").expect("[ERROR - ssid]");
+    // let password = heapless::String::from_str("password").expect("[ERROR - password]");
 
-    let ap_config = Configuration::AccessPoint(AccessPointConfiguration{
-        ssid,
-        password,
-        ..Default::default()
-    });
+    // let ap_config = Configuration::AccessPoint(AccessPointConfiguration{
+    //     ssid,
+    //     password,
+    //     ..Default::default()
+    // });
+   
+    
+    
+        
+    let cli_ssid = heapless::String::from_str("wifi_ssid").expect("[ERROR - cli_ssid]");
+    let cli_sec = heapless::String::from_str("wifi_sec").expect("[ERROR] - cli_sec");
+    let station_configuration = Configuration::Client(
+        ClientConfiguration{
+            ssid:cli_ssid,
+            password:cli_sec,
+            auth_method:AuthMethod::WPA2Personal,
+            ..Default::default()
+        }
+    );
+    // let _ = w_driver.set_configuration(&ap_config);
 
-    let _ = w_driver.set_configuration(&ap_config);
+    w_driver.set_configuration(&station_configuration).unwrap();
 
-    w_driver.start();
+    w_driver.start().unwrap();
+    w_driver.connect().unwrap();
+
+    while !w_driver.is_connected().unwrap(){
+        log::info!("Not Connected! {:?}",station_configuration);
+        delay.delay_ms(1000);
+    }
+        log::info!("Connected! {:?}",w_driver.sta_netif().get_ip_info());
 
 
     let mut led = PinDriver::output(pins.gpio2).unwrap();
